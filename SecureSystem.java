2d6214398a96438dd5142606b202a533052285a8
Olga <osaprych@gmail.com>
2013-09-06 17:51:51 -0500

    reading file!

--- SecureSystem.java
+++ SecureSystem.java
@@ -5,8 +5,16 @@ import java.io.*;
 
 class InstructionObject {
 
-	// public static void parseInput(Scanner in) throws IOException{
- //    }
+    public static void parseInput(Scanner in) throws IOException{
+        if (in.hasNext())
+            System.out.println("yay");
+        String s;
+        while(in.hasNext()){
+            s = in.nextLine();
+            System.out.println(s);
+        }
+
+    }
     public static void instrFunction() {
         System.out.println("InstructionObject!");
     }
@@ -25,10 +33,13 @@ class ObjectManager {
 }
 
 class SecureSystem {
-    public static void main(String[] args) {
-    	//Scanner inFile = new Scanner(new FileReader("instructionList.txt"));
-    	//InstructionObject instrObj = new InstructionObject();
-    	//instrObj.parseInput(inFile);
+    public static void main(String[] args) throws IOException{
+    	Scanner inFile = new Scanner(new FileReader("instructionList.txt"));
+
+        // if (inFile.hasNext())
+        //     System.out.println("yay");
+    	InstructionObject instrObj = new InstructionObject();
+    	instrObj.parseInput(inFile);
     	// SecureObject obj = new SecureObject();
     	// obj.objFunction();
     	// ObjectManager mObj = new ObjectManager();
