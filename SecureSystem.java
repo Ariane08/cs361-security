811a6a1eb0add0022266c298abe1e9deda7a6aae
Olga <osaprych@gmail.com>
2013-09-11 18:18:22 -0500

    in the middle of RM dev

--- SecureSystem.java
+++ SecureSystem.java
@@ -24,12 +24,10 @@ class InstructionObject {
 
             //Check second ele of instruction
             subjName = tokens[1];
-            //known subject of the secure system?
             System.out.println("subjName = " + subjName);
 
             //Check third ele of instruction
-            objName = tokens[2];
-            //known object of the secure system?       
+            objName = tokens[2];      
             System.out.println("objName = " + objName);
         }
         else {
@@ -41,7 +39,6 @@ class InstructionObject {
         if (type.equals("WRITE")){
             if (tokens.length == 4){
                 value = Integer.parseInt(tokens[3]);
-                //Print object variables
                 System.out.println("value = " + value);
             }
             else {
@@ -51,13 +48,13 @@ class InstructionObject {
         }
     }
 
-    public static void instrFunction() {
+    public static void instrMethod() {
         System.out.println("InstructionObject!\n");
     }
 }
 
 class BadInstruction {
-    public static void badInstructionFunction() {
+    public static void badInstruction() {
         System.out.println("BadInstruction!");
     }
 }
@@ -85,6 +82,7 @@ class Subject {
         //temp is initially zero
         temp = 0;
         level = inLevel;
+        //updateRM();
     }
 
     public static void read() {
@@ -128,43 +126,123 @@ class ObjectManager {
     }
 }
 
-class ReferenceMonitor {
+class Lookup {
+    // map subjName and objName (from instrObj) 2 objects in the program
+    private static Map<String, Subject> sMap = new HashMap<String, Subject>();
+    private static Map<String, SecureObject> oMap = new HashMap<String, SecureObject>();
 
-    //Handle the read/write requests of the subject
-    // 
-    public static void refMonFunction() {
-        System.out.println("ReferenceMonitor!");
+    public static void setSubj(String sSub, Subject sub) {
+        sMap.put(sSub, sub);
+    }
+    public static void getSubj(String s) {
+        //gets Subject object associated with name s
+        sMap.get(s);
+        System.out.println("looked up " + s + " as " + sMap.get(s) + "\n");
+    }
+
+    public static void setObj(String sObj, SecureObject obj) {
+        oMap.put(sObj, obj);
+    }
+    public static void getObj(String o) {
+        //gets Subject object associated with name s
+        oMap.get(o);
+        System.out.println("looked up " + o + " as " + oMap.get(o) + "\n");
     }
 }
 
+class ReferenceMonitor { 
+    private static Map<Subject, Integer> subjectMap = new HashMap<Subject, Integer>();
+    private static Map<SecureObject, Integer> objectMap = new HashMap<SecureObject, Integer>();
+
+    public static void updateSubjectRM(Subject s, Integer level) {
+        subjectMap.put(s, level);   
+    }
+    public static void updateObjectRM(SecureObject o, Integer level) {
+        objectMap.put(o, level);   
+    }
+
+    //BLP
+    public static void monitorInstruction(InstructionObject instrObj) {
+        if (instrObj.type.equals("READ")){
+            //SSP
+            //ssp(lookup.getSubj(instrObj.subjName), lookup.getObj(instrObj.objName)) ;
+        }
+        else if (instrObj.type.equals("WRITE")){
+            //*-Property
+            //starProperty(instrObj.subjName)
+        }
+        else{
+            System.out.println("BAD instruction send to RM");
+        }
+
+    }
+
+    public static void ssp(Subject s, SecureObject o) {
+        if (subjectMap.containsKey(s) && objectMap.containsKey(o)){
+            //**replace comparison with dominates method that we need to write
+            if (s.level >= o.level){
+                //allow access
+
+            }
+            else {
+                //Deny access
+                System.out.println("This instruction violates SSP\n");
+            }
+        }
+        System.out.println("ssp!");
+    } 
+
+    public static void starProperty() {
+        System.out.println("starProperty!");
+    }    
+
+    public static void refMonFunction() {
+        System.out.println("ReferenceMonitor!");
+    }
+
+}
+
+//Top level class?
 class SecureSystem {
-
-
 
     public static void main(String[] args) throws IOException{
     	Scanner inFile = new Scanner(new FileReader("instructionList.txt"));
 
         // SecurityLevel low  = SecurityLevel.LOW;
         // SecurityLevel high = SecurityLevel.HIGH;
-
         int low = 0;
         int high = 1;
 
+        ReferenceMonitor rm = new ReferenceMonitor();
+        //Lookup lookup = new Lookup();
+
+        //Make subjects known the the secure system
         Subject lyle = new Subject();
         lyle.createSubject("Lyle", low);
         System.out.println(lyle.name + " " + lyle.level + "\n");
+        //Hard association set in RM
+        rm.updateSubjectRM(lyle, low);
+        //Set in lookup
+        //rm.lookup.setSubj(lyle.name, lyle);
+
 
         Subject hal = new Subject();
         hal.createSubject("Hal", high);
         System.out.println(hal.name + " " + hal.level + "\n");
+        rm.updateSubjectRM(hal, high);
+        //lookup.setSubj(hal.name, hal);
 
+        //Make objects known to the secure system
         SecureObject lobj = new SecureObject();
         lobj.createNewObject("Lobj", low);
         System.out.println(lobj.name + " " + lobj.level + "\n");
+        rm.updateObjectRM(lobj, low);
 
         SecureObject hobj = new SecureObject();
         hobj.createNewObject("Hobj", high);
         System.out.println(hobj.name + " " + hobj.level + "\n");
+        rm.updateObjectRM(hobj, high);
+
 
         //Instructions are parsed from the list
         String s;
@@ -175,7 +253,9 @@ class SecureSystem {
             InstructionObject instrObj = new InstructionObject();
             instrObj.assignObjElements(s);
             //Print end of instruction object exclaimation
-            instrObj.instrFunction();
+            instrObj.instrMethod();
+
+            rm.monitorInstruction(instrObj);
 
         }
 
